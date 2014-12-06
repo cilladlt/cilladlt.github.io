a8d668df8351732fd088aaa8d3b8f7955140c366
russelljahn <russelljahn@gmail.com>
2014-12-06 12:54:50 -0800

    Switched over to using bower and sass.

---
+++ js/app.js
@@ -0,0 +1,45 @@
+
+Main();
+
+
+function Main() {
+    $(document).ready(function() {
+
+        $(document).foundation();
+        SetupScrollAnimation();
+
+        var $galleries = $(".js-packery");
+
+        var $container = $galleries.imagesLoaded(function() {
+            // initialize Packery after all images have loaded
+            $container.packery({
+                itemSelector: '.packery_item',
+                gutter: 15
+            });
+        });
+    });
+}
+
+
+function SetupScrollAnimation() {
+	// handle links with @href started with '#' only
+    $(document).on('click', 'a[href^="#"]', function (e) {
+        // target element id
+        var id = $(this).attr('href');
+
+        // target element
+        var $id = $(id);
+        if ($id.size() === 0) {
+            return;
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+
+        // top position relative to the document
+        var pos = $(id).offset().top;
+
+        // animated top scrolling
+        $('body, html').animate({scrollTop: pos, easing: "easeInOutQuad", duration: 1000});
+    });
+}
