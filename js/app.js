25215a56b2c950a733b6ff09350e9c07bcd67b89
russelljahn <russelljahn@gmail.com>
2014-12-09 02:12:08 -0800

    Gallery images fade in once loaded.

--- js/app.js
+++ js/app.js
@@ -1,49 +1,108 @@
 
+(function(window, document, $, undefined) {
 
-Main();
+    "use strict";
+    var $galleries;
+    var $conceptGalleries;
+    var $sketchGalleries;
+    var $printGalleries;
 
 
-function Main() {
-    $(document).ready(function() {
+    function setupScrollAnimation() {
+        var textOffset = -35;
+        // handle links with @href started with '#' only
+        $(document).on('click', 'a[href^="#"]', function (e) {
+            // target element id
+            var id = $(this).attr('href');
 
-        $(document).foundation();
-        SetupScrollAnimation();
+            // target element
+            var $id = $(id);
+            if ($id.size() === 0) {
+                return;
+            }
+            // prevent standard hash navigation (avoid blinking in IE)
+            e.preventDefault();
 
-        var $galleries = $(".js-packery");
+            // top position relative to the document
+            var pos = $(id).offset().top + textOffset;
+            // animated top scrolling
+            $('body, html').animate({ scrollTop: pos, easing: "easeInOutCubic", duration: 1000 });
+        });
+    }
 
-        var $container = $galleries.imagesLoaded(function() {
-            // initialize Packery after all images have loaded
-            $container.packery({
-                itemSelector: '.packery_item',
-                gutter: 15
+
+    function onLoadedImage(imagesLoadedInstance, image) {
+        //console.log("loaded: " + image);
+        var $img = $(image.img);
+        var $galleryItem = $img.parent();
+        $galleryItem.removeClass("loading");
+
+        $galleries.packery('once', 'layoutComplete', function () {
+            $img.velocity("fadeIn");
+            $galleryItem.velocity({
+                backgroundColor: "#000000",
+                backgroundColorAlpha: 0.0
             });
         });
+
+        $galleries.packery({
+            itemSelector: ".gallery-item",
+            gutter: 15
+        });
+    }
+
+
+    function main() {
+
+        $(document).foundation();
+        setupScrollAnimation();
+
+        $galleries = $(".gallery");
+        var $packeryGalleries = $(".js-packery");
+        $conceptGalleries = $(".concept-gallery");
+        $sketchGalleries = $(".sketch-gallery");
+        $printGalleries = $(".print-gallery");
+
+
+        var $packery = $packeryGalleries.packery({
+            itemSelector: ".gallery-item",
+            gutter: 15
+        });
+
+        window.galleryItems.forEach(function(elem) {
+            var dom = sprintf("<div class='gallery-item loading medium-%i'><img style='opacity:0;' src='%s'></div>", 
+                elem.size, elem.image);
+            //console.log(dom);
+
+            switch (elem.gallery) {
+                case "concept":
+                    $conceptGalleries.append($(dom));
+                    break;
+                case "sketch":
+                    $sketchGalleries.append($(dom));
+                    break;
+                case "print":
+                    $printGalleries.append($(dom));
+                    break;
+            }
+        });
+        $galleries.imagesLoaded().progress(onLoadedImage);
+        $galleries.packery({
+            itemSelector: ".gallery-item",
+            gutter: 15
+        });
+    }
+
+
+    /* Callback once DOM is loaded. */
+    $(document).ready(function () {
+        main();
     });
-}
 
 
-function SetupScrollAnimation() {
 
-    var textOffset = -35;
 
-	// handle links with @href started with '#' only
-    $(document).on('click', 'a[href^="#"]', function (e) {
-        // target element id
-        var id = $(this).attr('href');
+})(window, document, jQuery, undefined);
 
-        // target element
-        var $id = $(id);
-        if ($id.size() === 0) {
-            return;
-        }
 
-        // prevent standard hash navigation (avoid blinking in IE)
-        e.preventDefault();
 
-        // top position relative to the document
-        var pos = $(id).offset().top + textOffset;
-
-        // animated top scrolling
-        $('body, html').animate({scrollTop: pos, easing: "easeInOutCubic", duration: 1000});
-    });
-}
---
+++ js/portfolio.js
@@ -0,0 +1,88 @@
+
+
+(function(window) {
+
+    "use strict";
+
+    window.galleryItems = [
+        /* Concept */
+        {
+            name: "Dredz McGee",
+            image: "img/sultan/sl_dreds_001.jpg",
+            size: 4,
+            gallery: "concept"
+        },
+        {
+            name: "Link Kills Navi",
+            image: "img/zelda/zl_link.jpg",
+            size: 6,
+            gallery: "concept"
+        },
+        {
+            name: "King Crow Concept I",
+            image: "img/kingcrow/kc_kingcrow_concept_001.png",
+            size: 4,
+            gallery: "concept"
+        },
+        {
+            name: "King Crow Logo",
+            image: "img/kingcrow/kc_logo.png",
+            size: 4,
+            gallery: "concept"
+        },
+        /* Sketches */
+        {
+            name: "King Crow Sketch I",
+            image: "img/kingcrow/kc_kingcrow_blue_002.jpg",
+            size: 4,
+            gallery: "sketch"
+        },
+        {
+            name: "Princess Padle Sketch I",
+            image: "img/kingcrow/kc_padle_blue_001.png",
+            size: 4,
+            gallery: "sketch"
+        },
+        {
+            name: "Lady Luck Sketch I - Teen",
+            image: "img/ladyluck/ll_teen_blue_001.png",
+            size: 4,
+            gallery: "sketch"
+        },
+        {
+            name: "King Crow Sketch II",
+            image: "img/kingcrow/kc_kingcrow_blue_001.jpg",
+            size: 3,
+            gallery: "sketch"
+        },
+        {
+            name: "King Crow Sketch III",
+            image: "img/kingcrow/kc_kingcrow_blue_003.png",
+            size: 3,
+            gallery: "sketch"
+        },
+        /* Prints */
+        {
+            name: "Fall of Lindblum",
+            image: "img/prints/print_lindblum.jpg",
+            size: 4,
+            gallery: "print"
+        },
+        {
+            name: "Owl",
+            image: "img/prints/print_owl.jpg",
+            size: 3,
+            gallery: "print"
+        },
+        {
+            name: "Lamia",
+            image: "img/prints/print_snake.jpg",
+            size: 4,
+            gallery: "print"
+        }
+    ];
+
+})(window);
+
+
+
